09c480011017a8d3df35e60535f417177d3b79e0
PeaStew <peter.gavin.stewart@gmail.com>
2022-01-28 01:02:54 +0100

    built some more packages

--- Nfantom.JsonRpc.IpcClient/UnixDomainSocketEndPoint.cs
+++ Nfantom.JsonRpc.IpcClient/UnixDomainSocketEndPoint.cs
@@ -8,7 +8,7 @@ namespace Nfantom.JsonRpc.IpcClient
 {
     // Licensed to the .NET Foundation under one or more agreements.
     // The .NET Foundation licenses this file to you under the MIT license.
-    // See https://github.com/dotnet/corefx/blob/master/LICENSE.TXT
+    // See https://github.com/dotnet/corefx/blob/master/LICENSE.txt
 
     //This is part of Dotnet Core, waiting to be released externally. NamedPipesStream uses it but it does not allow for paths.
 
